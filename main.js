23d328ae04729cc199bac6536118b7445b6a890b
slpkid <153894144+slpkid@users.noreply.github.com>
2024-05-02 23:23:33 -0700

    Add winstate logic to complete console tic-tac-toe

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ function Gameboard() {
     columns = 3
     rows = 3
     board = []
+    playable = true
+    let winner
     let currentPlayer = "X"
 
     for (let i = 0; i < rows; i++) {
@@ -21,6 +23,10 @@ function Gameboard() {
     }
 
     const playRound = (row,col) => {
+        if (playable === false) {
+            console.log(`Player ${winner} won the game. Run game.clearBoard() to play again...`)
+            return
+        }
         if (row > 2 || col > 2) {
             console.log("Invalid move!")
             return
@@ -33,8 +39,12 @@ function Gameboard() {
         board[row][col] = currentPlayer;
         changePlayer();
         console.log(board);
-        console.log(`Player ${currentPlayer}'s turn...`)
+        hasWon();
+        if (playable === true) {
+            console.log(`Player ${currentPlayer}'s turn...`)
+        } else {
         return
+        }
     }
 
     const playableSpaces = () => {
@@ -48,18 +58,61 @@ function Gameboard() {
         if (playableSpaces.length === 0) {
             console.log("Draw! Please start a new game.")
         }
+        return;
     };
+
+    const getState = (input) => {
+        if (input === "XXX") {
+            winner = "X"
+            playable = false;
+            console.log("Player X won!")
+            return
+        } else if (input === "OOO") {
+            winner = "O"
+            playable = false;
+            console.log("Player O won!")
+            return
+        }
+        return
+    }
+
+    const hasWon = () => {
+        //rows
+        for (let i = 0; i < rows; i++) {
+            let winner = "";
+            for (let j = 0; j < columns; j++) {
+                winner += `${board[i][j]}`;
+            }
+            getState(winner);
+        }
+        //columns
+        for (let j = 0; j < columns; j++) {
+            let winner = "";
+            for (let i = 0; i < rows; i++) {
+                winner += `${board[i][j]}`;
+            }
+            getState(winner);
+        }
+        //diagonal hell
+        (function () {
+            let winner = `${board[0][0]}${board[1][1]}${board[2][2]}`
+            getState(winner);
+            winner = `${board[0][2]}${board[1][1]}${board[2][0]}`
+            getState(winner);
+        })()
+
+    }
 
     const fillBoard = () => {
         playRound(0,0);
-        playRound(0,1);
-        playRound(0,2);
-        playRound(1,0);
-        playRound(1,1);
         playRound(1,2);
+        playRound(0,1);
+        playRound(2,2);
+        playRound(0,2);
+        playRound(1,1);
+        playRound(1,0);
         playRound(2,0);
         playRound(2,1);
-        playRound(2,2);
     }
 
     const getBoard = () => {
@@ -67,15 +120,17 @@ function Gameboard() {
     }
 
     const clearBoard = () => {
+        console.log("Board cleared. Starting a new game...");
+        playable = true;
+        winner = "";
         Gameboard();
-        console.log("Board cleared. Starting a new game...")
         return;
     }
 
     console.log(board)
     console.log(`Player ${currentPlayer}'s turn...`)
 
-    return {playRound, getBoard, clearBoard, playableSpaces, fillBoard}
+    return {playRound, getBoard, clearBoard, playableSpaces, fillBoard, getState, hasWon}
 }
 
 const game = Gameboard();
